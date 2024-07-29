d87db6d6758c49a3c2a980559b7a7505b5c98883
fadlyan999 <fadlyan999@gmail.com>
2024-07-29 07:01:03 +0000

    deployed

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x042a11cDBFbC0b68C1d98395F10522Ed4694e5CF'
+export const ProxyAdmin = '0x412ba7f0fB5580ad11FAa16cB7790B483F7960f8'
+export const SWTRImplementation = '0xE02628d7F12cA69dc94Ad31455cED63b3c41c3D1'
     
